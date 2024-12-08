735603dba7091c0eb2d0e60a2baaafbad2004711
s0roh <sashalitovkin04@gmail.com>
2024-12-08 20:25:21 +0700

    ref: правки в ReminderMapper.kt

--- data/data-reminder/src/main/kotlin/com/example/data/reminder/data/mapper/ReminderMapper.kt
+++ data/data-reminder/src/main/kotlin/com/example/data/reminder/data/mapper/ReminderMapper.kt
@@ -3,7 +3,7 @@ package com.example.data.reminder.data.mapper
 import com.example.data.reminder.domain.entity.Reminder
 import com.example.database.models.TrainingReminderDBO
 
-fun Reminder.toDBO(): TrainingReminderDBO = TrainingReminderDBO(
+internal fun Reminder.toDBO(): TrainingReminderDBO = TrainingReminderDBO(
     id = id,
     reminderTime = reminderTime,
     name = name,
@@ -11,7 +11,7 @@ fun Reminder.toDBO(): TrainingReminderDBO = TrainingReminderDBO(
     deckId = deckId
 )
 
-fun TrainingReminderDBO.toEntity() = Reminder(
+internal fun TrainingReminderDBO.toEntity() = Reminder(
     id = id,
     reminderTime = reminderTime,
     name = name,
