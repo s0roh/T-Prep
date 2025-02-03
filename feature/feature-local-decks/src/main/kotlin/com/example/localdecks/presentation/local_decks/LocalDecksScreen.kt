3fea219b61c431894433b1766056b6cd5a96da6a
s0roh <sashalitovkin04@gmail.com>
2025-02-03 18:51:55 +0700

    feat: добавлен pull to refresh indicator для LocalDecksScreen

--- feature/feature-local-decks/src/main/kotlin/com/example/localdecks/presentation/local_decks/LocalDecksScreen.kt
+++ feature/feature-local-decks/src/main/kotlin/com/example/localdecks/presentation/local_decks/LocalDecksScreen.kt
@@ -1,8 +1,6 @@
 package com.example.localdecks.presentation.local_decks
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,50 +9,64 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.common.ui.DeckCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LocalDecksScreen(
-    paddingValues: PaddingValues,
     onDeckClick: (String) -> Unit,
     onAddClick: () -> Unit,
 ) {
     val viewModel: LocalDecksViewModel = hiltViewModel()
     val decks by viewModel.decks.collectAsState()
+    val isRefreshing by viewModel.isRefreshing.collectAsState()
 
-    Column {
-        TopAppBar(
-            title = { Text("Мои колоды") },
-            actions = {
-                IconButton(
-                    onClick = onAddClick
-                ) {
-                    Icon(Icons.Filled.Add, contentDescription = "Add Deck")
+    val context = LocalContext.current
+    val refreshState = rememberPullToRefreshState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Мои колоды") },
+                actions = {
+                    IconButton(
+                        onClick = onAddClick
+                    ) {
+                        Icon(Icons.Filled.Add, contentDescription = "Add Deck")
+                    }
                 }
-            }
-        )
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = paddingValues.calculateBottomPadding()),
+            )
+        }
+    ) { paddingValues ->
+        PullToRefreshBox(
+            modifier = Modifier.padding(paddingValues),
+            state = refreshState,
+            isRefreshing = isRefreshing,
+            onRefresh = { viewModel.startSync(context) }
         ) {
-            items(
-                items = decks,
-                key = { it.id }
-            ) { deck ->
-                DeckCard(
-                    deck = deck,
-                    modifier = Modifier.animateItem(),
-                    onDeckClickListener = { onDeckClick(deck.id) })
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(
+                    items = decks,
+                    key = { it.id }
+                ) { deck ->
+                    DeckCard(
+                        deck = deck,
+                        modifier = Modifier.animateItem(),
+                        onDeckClickListener = { onDeckClick(deck.id) })
+                }
             }
         }
     }
--- feature/feature-local-decks/src/main/kotlin/com/example/localdecks/presentation/local_decks/LocalDecksViewModel.kt
+++ feature/feature-local-decks/src/main/kotlin/com/example/localdecks/presentation/local_decks/LocalDecksViewModel.kt
@@ -1,23 +1,48 @@
 package com.example.localdecks.presentation.local_decks
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.example.common.domain.entity.Deck
 import com.example.localdecks.domain.usecase.GetDecksFlowUseCase
+import com.example.localdecks.util.startSyncWork
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 internal class LocalDecksViewModel @Inject constructor(
-    getDecksFlowUseCase: GetDecksFlowUseCase
+    getDecksFlowUseCase: GetDecksFlowUseCase,
 ) : ViewModel() {
+
+    var isRefreshing = MutableStateFlow<Boolean>(false)
+        private set
 
     val decks: StateFlow<List<Deck>> = getDecksFlowUseCase().stateIn(
         viewModelScope,
         SharingStarted.Lazily,
         emptyList()
     )
+
+    fun startSync(context: Context) {
+        viewModelScope.launch {
+            isRefreshing.value = true
+
+            val workId = startSyncWork(context)
+            val workManager = WorkManager.getInstance(context)
+
+            workManager.getWorkInfoByIdFlow(workId)
+                .collect { workInfo ->
+                    if (workInfo?.state == WorkInfo.State.SUCCEEDED || workInfo?.state == WorkInfo.State.FAILED) {
+                        isRefreshing.value = false
+                    }
+                }
+        }
+    }
 }
